60aedcf703cdd00316df93f24f4c081dba4af79c
Miguel Alonso Jr <miguel.alonso@unity3d.com>
2023-05-30 16:40:53 -0400

    Added pydoc-markdown API doc autogen precommit hook.

---
+++ docs/data-api-documentation.md
@@ -0,0 +1,17 @@
+# Table of Contents
+
+* [anyGPT.data.experience\_dataset](#anyGPT.data.experience_dataset)
+* [anyGPT.data.next\_token\_dataset](#anyGPT.data.next_token_dataset)
+* [anyGPT.data.prepare\_data](#anyGPT.data.prepare_data)
+
+<a id="anyGPT.data.experience_dataset"></a>
+
+# anyGPT.data.experience\_dataset
+
+<a id="anyGPT.data.next_token_dataset"></a>
+
+# anyGPT.data.next\_token\_dataset
+
+<a id="anyGPT.data.prepare_data"></a>
+
+# anyGPT.data.prepare\_data
--- tests/test_service.py
+++ tests/test_service.py
@@ -10,11 +10,21 @@ def test_notfound():
     response = client.get("/")
     assert response.status_code == 404
 
+
 def test_badrequest():
     response = client.post("/infer", json={"fake": "hello world"})
     assert response.status_code == 422
 
+
 def test_infer():
-    response = client.post("/infer", json={"data": "hello world", "temperature": 0.5, "top_k": 150, "max_new_tokens": 450})
+    response = client.post(
+        "/infer",
+        json={
+            "data": "hello world",
+            "temperature": 0.5,
+            "top_k": 150,
+            "max_new_tokens": 450,
+        },
+    )
     assert response.status_code == 200
     assert response.text != ""
